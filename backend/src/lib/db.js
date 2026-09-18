af336d5da92f0c76613815f5262a8a19768b0cf1
Shivanshu Singh <shivamsinghshivam360@gmail.com>
2026-09-18 22:39:07 +0530

    not found error fixed

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -6,6 +6,9 @@ dns.setServers(['8.8.8.8', '8.8.4.4'])
 
 export const connectDB = async() => {
     try{
+        if(!ENV.MONGO_URI){
+            throw new Error("MONGO_URI is not defined in environment variables")
+        }
         const conn = await mongoose.connect(ENV.MONGO_URI)
         console.log("Connected to MongoDb:",conn.connection.host)
     }catch(error){
