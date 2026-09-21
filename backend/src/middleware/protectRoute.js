6de319ebcafd7f7cf895523417a86aebb744ce2f
Shivanshu Singh <shivamsinghshivam360@gmail.com>
2026-09-21 21:08:05 +0530

    minor typo fixes

--- backend/src/middleware/protectRoute.js
+++ backend/src/middleware/protectRoute.js
@@ -6,13 +6,13 @@ export const protectRoute = [
     async (req,res,next) => {
         try {
             const clerkId = req.auth().userId;
-            if(!clerkId) return res.status(401).json({msg: "Unauthorized - invalid token"})
+            if(!clerkId) return res.status(401).json({message: "Unauthorized - invalid token"})
             
             //find user in db by clerk id
             const user = await User.findOne({clerkId:clerkId})
 
             if(!user){
-                return res.status(404).json({msg:"User not found"})
+                return res.status(404).json({message:"User not found"})
             }
 
             //attach user to req
